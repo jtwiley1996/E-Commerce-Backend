17fd8334ce344cf264edd800ae847fdbe7fc6d3f
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-03-04 21:34:32 -0600

    server.js main

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const routes = require('./Starter-Folder/routes');
-// import sequelize connection
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+const cfonts = require('cfonts');
+// import connections
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +12,23 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// syncing sequelize models to the database, and turning on the server
+sequelize.sync({ force: false}).then(() => {
+  app.listen(PORT, () =>console.log(`App listening on port ${PORT}!`));
+});
+
+//Showing the name of the App
+cfonts.say('CYBERMARKET', {
+	font: 'block',              // define the font face
+	align: 'center',              // define text alignment
+	colors: ['white'],         // define all colors
+	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+	letterSpacing: 1,           // define letter spacing
+	lineHeight: 1,              // define the line height
+	space: true,                // define if the output text should have empty lines on top and on the bottom
+	maxLength: '0',             // define how many character can be on one line
+	gradient: false,            // define your two gradient colors
+	independentGradient: false, // define if you want to recalculate the gradient for each new line
+	transitionGradient: false,  // define if this is a transition between colors directly
+	env: 'node'                 // define the environment cfonts is being executed in
 });
